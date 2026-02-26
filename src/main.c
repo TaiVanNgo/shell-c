0854ce80b9ba9ee0a1b16b5eccef78721a40b0b6
TaiVanNgo <vantaingo.056@gmail.com>
2026-02-26 08:51:17 +0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+
+  char command[1024];
+  if (fgets(command, sizeof(command), stdin))
+  {
+    // find "\n" position
+    size_t pos = strcspn(command, "\n");
+    command[pos] = '\0'; // replace it with terminal char
+
+    printf("%s: command not found", command);
+  }
 
   return 0;
 }
